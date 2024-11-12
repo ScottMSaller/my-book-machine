9bbca2848345c925031406fff29e030651c86be1
Scott Saller <scottmsaller@gmail.com>
2024-11-12 12:21:17 -0500

    hotfix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   build: {
     outDir: 'dist',
   },
-  
+
   plugins: [react()],
   envDir: '.',
   server: {
