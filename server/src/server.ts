b3d0bec5067a10d65988682eb1cfb72b111074c2
Scott Saller <scottmsaller@gmail.com>
2024-11-12 12:00:49 -0500

    hotfix

--- server/src/server.ts
+++ server/src/server.ts
@@ -30,10 +30,10 @@ app.use(express.json());
 
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
 });
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
