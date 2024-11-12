f6f656552973f9e711d82440133cedaf92329653
Scott Saller <scottmsaller@gmail.com>
2024-11-11 23:37:49 -0500

    more hotfix

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,7 +25,13 @@ app.use(cors({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 
+// Catchall handler for React routes (only for non-API routes)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (_req, res) => {
